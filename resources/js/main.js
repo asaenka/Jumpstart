a6163789dab2e99b723419eb6f94a8898d6c08f7
Maria Asayonak <maria.asayonak@gmail.com>
2017-06-20 17:34:09 +0200

    refactor css

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,7 +4,27 @@ $(function() {
     appendDots: false,
     appendArrows: false,
     autoplay: true,
-    autoplaySpeed: 3000
+    autoplaySpeed: 3000000
+  });
+
+  var $navbar = $(".navbar");
+  var navbarHeight = $navbar.css('height');
+  var navbarVisible = true;
+
+  $(window).on("scroll", function() {
+    var scrollY = $(this).scrollTop();
+
+    if(scrollY > 50) {
+      if(navbarVisible) {
+        $navbar.animate({top: '-' + navbarHeight}, 100);
+        navbarVisible = false;
+      }
+    } else {
+      if(!navbarVisible) {
+        $navbar.animate({top: "0px"}, 100);
+        navbarVisible = true;
+      }
+    }
   });
 });
 
